78945ecd913db989b44bb46cc7278782363e8eca
jaykeblakk <cobycarter@gmail.com>
2025-10-12 01:01:03 -0600

    Update main.tf
    Added the EKS creation to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,27 @@ module "vpc" {
 
   enable_dns_hostnames    = true
 }
+
+module "eks" {
+    source = "terraform-aws-modules/eks/aws"
+    version = "20.8.5"
+    cluster_name = "writing_analyzer_cluster"
+    cluster_version = "1.29"
+    cluster_endpoint_public_access = true
+    enable_cluster_creator_admin_permissions = true
+    vpc_id     = module.vpc.vpc_id
+    subnet_ids = module.vpc.private_subnets
+    eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+    }
+    eks_managed_node_groups = {
+        one = {
+            name = "node-group-1"
+            instance_types = ["t2.micro"]
+            min_size = 1
+            max_size = 2
+            desired_size = 1
+        }
+    }
+}
