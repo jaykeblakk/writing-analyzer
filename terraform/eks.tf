57b3df25f31c085152b1bc1ef85d580d9ff049cd
jaykeblakk <cobycarter@gmail.com>
2025-10-21 15:52:43 -0600

    Update eks.tf again
    Change the AMI type

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -27,7 +27,7 @@ module "eks" {
   }
 
   eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
+    ami_type       = "AL2023_x86_64_STANDARD"
     instance_types = ["t2.micro"]
     
     # Use custom IAM role for nodes
