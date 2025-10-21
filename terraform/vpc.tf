3a0e53aa19b4634854de967988886c3c3af7d75d
jaykeblakk <cobycarter@gmail.com>
2025-10-21 14:36:19 -0600

    Update vpc.tf
    Added nat gateways to VPC for the EKS cluster instances to connect to the nodes.

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,6 +9,8 @@ module "vpc" {
   private_subnets = var.vpc_private_subnets
   public_subnets  = var.vpc_public_subnets
 
+  enable_nat_gateway = true
+  single_nat_gateway = true
   enable_dns_hostnames = var.vpc_enable_dns_hostnames
 }
 
