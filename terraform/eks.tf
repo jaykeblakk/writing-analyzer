16a79d3a8f9c293c57093dcb93b5aeffa1cc3042
jaykeblakk <cobycarter@gmail.com>
2025-11-18 23:53:15 -0700

    Update eks.tf
    Remove bad tags

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -7,7 +7,7 @@ module "eks" {
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.public_subnets
-  
+
   enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
@@ -25,6 +25,4 @@ module "eks" {
       }
     }
   }
-
-  tags = local.tags
 }
