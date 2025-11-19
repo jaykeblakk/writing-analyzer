b9a476737f22a026c6f40ae1129832320eb326c1
jaykeblakk <cobycarter@gmail.com>
2025-11-18 23:30:27 -0700

    Split tf files
    Split the tf file into multiple. Set it up to be exactly like an example. If this fails, there is something else going on

---
+++ terraform/eks.tf
@@ -0,0 +1,31 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.8.0"
+  kubernetes_version = "1.34"
+
+  name                   = "test-cluster"
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.public_subnets
+
+  cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    test-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.micro"]
+      capacity_type  = "SPOT"
+      attach_cluster_primary_security_group = true
+
+      tags = {
+        ExtraTag = "testtag"
+      }
+    }
+  }
+
+  tags = local.tags
+}
