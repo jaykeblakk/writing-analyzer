605b6f9df9a004b7099d233e4a37eef326f9ed28
jaykeblakk <cobycarter@gmail.com>
2025-11-19 00:47:52 -0700

    Update eks.tf
    Set EKS file to match the terraform tutorial.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -8,7 +8,14 @@ module "eks" {
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.public_subnets
 
+  cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+    }
+  }
 
   eks_managed_node_groups = {
     test-cluster-wg = {
@@ -25,4 +32,19 @@ module "eks" {
       }
     }
   }
+}
+
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
