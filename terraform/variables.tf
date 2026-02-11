5bab7ae7b87d9a3bc1e4a20a6594750d0a222e4a
jaykeblakk <cobycarter@gmail.com>
2026-02-10 22:48:32 -0700

    Add region
    Added region to aws provider

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-west-1"
+}
+
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
